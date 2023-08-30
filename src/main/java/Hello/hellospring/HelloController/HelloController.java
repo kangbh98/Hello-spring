27036851d3bc8eefbc1475c261e83d42c0f9e1ab
BH kang <rkdqudgjs1127@gmail.com>
2023-08-30 17:52:16 +0900

    변경

--- src/main/java/Hello/hellospring/HelloController/HelloController.java
+++ src/main/java/Hello/hellospring/HelloController/HelloController.java
@@ -3,6 +3,8 @@ package Hello.hellospring.HelloController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,36 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody // http 에서 body 부분에 리턴값을 직접 넣겠다는 뜻
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; // hello spring 같은걸로 바뀜 이거는 문자가 그대로 내려간다 뷰같은거 없이
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
