ba839916607a86cca8ada7e5308e79a68325dca9
BH kang <rkdqudgjs1127@gmail.com>
2023-09-03 23:34:29 +0900

    변경

--- src/main/java/Hello/hellospring/Controller/MemberController.java
+++ src/main/java/Hello/hellospring/Controller/MemberController.java
@@ -1,8 +1,14 @@
 package Hello.hellospring.Controller;
 
 import Hello.hellospring.Service.MemberService;
+import Hello.hellospring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -10,6 +16,27 @@ public class MemberController {
 
     @Autowired
     public MemberController(MemberService memberService) {
+
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
---
+++ src/main/java/Hello/hellospring/Controller/MemberForm.java
@@ -0,0 +1,14 @@
+package Hello.hellospring.Controller;
+
+public class MemberForm {
+
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
